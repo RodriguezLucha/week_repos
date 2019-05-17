2424f4422039b3092e17d0487fe322ec274212d7
Rudy <RodriguezLucha@gmail.com>
2019-05-17 00:14:31 -0700

    Add W6D3

---
+++ W6D3/AJAXTwitter/frontend/follow_toggle.js
@@ -0,0 +1,48 @@
+const APIUtil = require('./api_util');
+
+class FollowToggle {
+    constructor($el) {
+        this.userId = $el.data('userId') ;
+        this.followState = $el.data('initialFollowState');
+        this.$el = $el;
+        this.render($el);
+        this.$el.on ('click', this.handleClick.bind(this));
+    };
+    
+    render($el){
+        if(this.followState === 'followed'){
+            $el.text("Unfollow!!");
+        }else if (this.followState === 'unfollowed'){
+            $el.text("Follow!");
+        }else if(this.followState === 'following'){
+            $el.text("unfollowing");
+        }else if(this.followState === 'unfollowing'){
+            $el.text("following");
+        }
+
+    };
+    
+    handleClick(e){
+        e.preventDefault();
+        if(this.followState === 'followed'){
+            this.followState = 'following';
+            this.render(this.$el);
+            APIUtil.unfollowUser(this.userId).then(()=>{
+                this.followState  = this.followState === 'following' ? 'unfollowed' : 'followed'
+                this.render(this.$el);
+            })
+        } else {
+            this.followState = 'unfollowing';
+            this.render(this.$el);
+            APIUtil.followUser(this.userId).then(() => {
+                this.followState = this.followState === 'unfollowing' ? 'unfollowed' : 'followed'
+                this.render(this.$el);
+            });
+        }
+        
+
+    }
+
+} 
+
+module.exports = FollowToggle;
